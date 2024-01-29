b53d71f5c4a69d65c067c88629b6b285f5e4ab07
AymanSdk <ayman.sdk123@oulook.com>
2024-01-29 17:40:05 +0100

    Refactor code to improve readability and efficiency

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""Gather data from an API"""
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
-
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    to_do = requests.get(url + "todos?userId={}".format(sys.argv[1])).json()
-    
-    
-    completed_tasks = [t.get("title") for t in to_do if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(user.get("name"), len(completed_tasks), len(to_do)))
-    [print("\t {}".format(task)) for task in completed_tasks]
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
