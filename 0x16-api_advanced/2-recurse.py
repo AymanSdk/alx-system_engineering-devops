24d1ab29053f2ae0c2af7c90c88093a47c2401c8
AymanSdk <ayman.sdk123@oulook.com>
2024-02-13 18:51:13 +0100

    Add Python script for retrieving subreddit hotlist

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# get subs
+from requests import get
+from sys import argv
+
+
+def recurse(subreddit, hotlist=[], after=None):
+    """subs"""
+    head = {'User-Agent': 'Psychological-Papi'}
+    try:
+        if after:
+            count = get('https://www.reddit.com/r/{}/hot.json?after={}'.format(
+                subreddit, after), headers=head).json().get('data')
+        else:
+            count = get('https://www.reddit.com/r/{}/hot.json'.format(
+                subreddit), headers=head).json().get('data')
+        hotlist += [dic.get('data').get('title')
+                    for dic in count.get('children')]
+        if count.get('after'):
+            return recurse(subreddit, hotlist, after=count.get('after'))
+        return hotlist
+    except Exception as e:
+        return None
+
+
+if __name__ == "__main__":
+    recurse(argv[1])
