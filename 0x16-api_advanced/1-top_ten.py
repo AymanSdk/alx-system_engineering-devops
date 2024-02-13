dc9c2f3f6939e0060503299c0217bed1154f002c
AymanSdk <ayman.sdk123@oulook.com>
2024-02-13 18:41:54 +0100

    Add top_ten function to retrieve top 10 posts from a subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# get subs
+from requests import get
+from sys import argv
+
+
+def top_ten(subreddit):
+    """subs"""
+    head = {'User-Agent': 'Psychological-Papi'}
+    try:
+        count = get('https://www.reddit.com/r/{}/hot.json?count=10'.format(
+            subreddit), headers=head).json().get('data').get('children')
+        print('\n'.join([dic.get('data').get('title')
+                         for dic in count][:10]))
+    except:
+        print('None')
+
+
+if __name__ == "__main__":
+    top_ten(argv[1])
