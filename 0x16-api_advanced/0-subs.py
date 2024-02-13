d2edaed37a8bd3a7c093ffd9d0998358bddb1c6a
AymanSdk <ayman.sdk123@oulook.com>
2024-02-13 19:08:45 +0100

    Update user-agent header in API request

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    headers = {'user-agent': 'psychological-papi'}
+    headers = {'user-agent': 'my-app/0.0.1'}
 
     r = get(url, headers=headers, allow_redirects=False)
 
